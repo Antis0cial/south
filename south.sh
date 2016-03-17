27e7ed5cfdc0bbabfc4ffcf3df2a9641ce595d95
Antis0cial <antis0cial@pwnt.eu>
2016-03-17 20:55:53 +0100

    bullshit refactored

--- south.sh
+++ south.sh
@@ -4,10 +4,4 @@ if [ $# -eq 0 ]; then exit 0; fi
 
 if [ ! -x "$1" ]; then chmod u+x $1; fi
 
-tmp=".tmp-out"
-rm -f $tmp
-./$@ 1>${tmp}
-echo -ne "\e[7m"
-cat $tmp
-echo -ne "\e[27m"
-rm -f $tmp
+echo -ne "\e[7m"; ./$@; echo -ne "\e[27m"
